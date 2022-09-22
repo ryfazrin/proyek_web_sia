5d4fa1bc6facc8861a9b398afa08744a57127dd7
ryfazrin <riansera515@gmail.com>
2022-09-22 09:03:08 +0700

    updated: controller

--- app/Http/Controllers/SegiEmpatController.php
+++ app/Http/Controllers/SegiEmpatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use app\Models\SegiEmpat;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\View;
 
 class SegiEmpatController extends Controller
 {

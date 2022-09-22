0e00cf141e35a6b0cd6fe935fb09c218044d596d
ryfazrin <riansera515@gmail.com>
2022-09-22 09:12:08 +0700

    fix: change namespace model

--- app/Http/Controllers/SegiEmpatController.php
+++ app/Http/Controllers/SegiEmpatController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\SegiEmpat;
+use App\Models\SegiEmpat;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\View;
 

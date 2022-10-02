2a69c51d5b40abe5f6169ead8f969d1eaaffdddf
ryfazrin <riansera515@gmail.com>
2022-10-02 14:29:14 +0700

    added: MstPangkat Controller

---
+++ app/Http/Controllers/MstPangkatController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MstPangkat;
+use Illuminate\Http\Request;
+
+class MstPangkatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $search = \Request::get('search');
+        $p = MstPangkat::paginate();
+
+        $mstPangkat = \DB::table('mst_pangkat')
+            ->where('nama_pangkat', 'LIKE', '%' . $search . '%')
+            ->orWhere('pangkat_gol', 'LIKE', '%' . $search . '%')
+            ->paginate($p->perPage());
+
+        return view('mst-pangkat.index', compact('mstPangkat'))
+            ->with('i', (request()->input('page', 1) - 1) * $p->perPage());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}

e34844452874bb945507252879e0f7cb3188cf74
ryfazrin <riansera515@gmail.com>
2022-10-06 07:53:27 +0700

    CRUD mst-pangkat

--- app/Http/Controllers/MstPangkatController.php
+++ app/Http/Controllers/MstPangkatController.php
@@ -33,7 +33,8 @@ class MstPangkatController extends Controller
      */
     public function create()
     {
-        //
+        $mstPangkat = new MstPangkat();
+        return view('mst-pangkat.create', compact('mstPangkat'));
     }
 
     /**
@@ -44,7 +45,31 @@ class MstPangkatController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // cel validasi
+        request()->validate(MstPangkat::$rules);
+        //mulai transaksi
+        \DB::beginTransaction();
+        try {
+            //simpan ke tabel kota
+            \DB::table('mst_pangkat')->insert([
+                'nama_pangkat' => $request->nama_pangkat,
+                'pangkat_gol' => $request->pangkat_gol,
+                'created_at' => date('Y-m-d H:m:s'),
+                'updated_at' => date('Y-m-d H:m:s')
+            ]);
+            //jika tidak ada kesalahan commit/simpan
+            \DB::commit();
+            return redirect()->route('mst-pangkat.index')
+                ->with('success', 'Master Tabel Pangkat created successfully.');
+        } catch (\Throwable $e) {
+            //jika terjadi kesalahan batalkan semua
+            \DB::rollback();
+            return redirect()->route('mst-pangkat.index')
+                ->with(
+                    'success',
+                    'Penyimpanan dibatalkan semua, ada kesalahan......'
+                );
+        }
     }
 
     /**
@@ -55,7 +80,8 @@ class MstPangkatController extends Controller
      */
     public function show($id)
     {
-        //
+        $mstPangkat = MstPangkat::find($id);
+        return view('mst-pangkat.show', compact('mstPangkat'));
     }
 
     /**
@@ -66,7 +92,8 @@ class MstPangkatController extends Controller
      */
     public function edit($id)
     {
-        //
+        $mstPangkat = MstPangkat::find($id);
+        return view('mst-pangkat.edit', compact('mstPangkat'));
     }
 
     /**
@@ -78,7 +105,27 @@ class MstPangkatController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        request()->validate(MstPangkat::$rules);
+        //mulai transaksi
+        \DB::beginTransaction();
+        try {
+            \DB::table('mst_pangkat')->where('id', $id)->update([
+                'nama_pangkat' => $request->nama_pangkat,
+                'pangkat_gol' => $request->pangkat_gol,
+                'updated_at' => date('Y-m-d H:m:s')
+            ]);
+            \DB::commit();
+            return redirect()->route('mst-pangkat.index')
+                ->with('success', 'MstPangkat updated successfully');
+        } catch (\Throwable $e) {
+            //jika terjadi kesalahan batalkan semua
+            \DB::rollback();
+            return redirect()->route('mst-pangkat.index')
+                ->with(
+                    'success',
+                    'Tabel Pangkat Batal diubah, ada kesalahan'
+                );
+        }
     }
 
     /**
@@ -89,6 +136,22 @@ class MstPangkatController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //mulai transaksi
+        \DB::beginTransaction();
+        try {
+            //hapus rekaman tabel kota
+            $mstPangkat = MstPangkat::find($id)->delete();
+            \DB::commit();
+            return redirect()->route('mst-pangkat.index')
+                ->with('success', 'Tabel Pangkat deleted successfully');
+        } catch (\Throwable $e) {
+            //jika terjadi kesalahan batalkan semua
+            \DB::rollback();
+            return redirect()->route('mst-pangkat.index')
+                ->with(
+                    'success',
+                    'Tabel Pangkat ada kesalahan hapus batal... '
+                );
+        }
     }
 }
